0213576d44d0aa0043304339488c8e980a0e83bd
kayak <kayak@kayak.rocks>
2020-11-08 12:15:38 +1000

    xrandr: add script to set up monitors
    added for the sake of completion

---
+++ home/.screenlayout/screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x600 --rotate normal --output HDMI2 --off --output HDMI3 --mode 1680x1050 --pos 1920x0 --rotate left --output VIRTUAL1 --off
