e2af69aa75150eead795a905dc9bd332c39493c8
kayak <kayak@kayak.rocks>
2021-05-17 20:25:56 +1000

    fish: add cargo install location to PATH

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -13,6 +13,8 @@ set PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $AND
 
 set PATH ~/.npm-global/bin /home/kayak/.local/bin /home/kayak/bin $PATH
 
+set PATH ~/.cargo/bin $PATH
+
 # fix ruby
 set PATH $PATH (ruby -e 'puts Gem.user_dir')/bin
 
